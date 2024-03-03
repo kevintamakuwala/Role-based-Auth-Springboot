fc509a936fe91850e444d7dbbfd1010846a41925
sahilsapariya <sahilsapariya03@gmail.com>
2024-03-04 00:48:21 +0530

    labs resources page done

--- client/src/pages/LabsResources.js
+++ client/src/pages/LabsResources.js
@@ -1,9 +1,47 @@
-import React from 'react'
+import React from "react";
+import useFetch from "../hooks/useFetch";
+import "../styles/Labs.scss";
+import { baseurl } from "../config";
 
 const LabsResources = () => {
-  return (
-    <div>LabsResources</div>
-  )
-}
+  const { data: labs, loading, error } = useFetch(`${baseurl}/labs`);
 
-export default LabsResources
+  if (loading) {
+    return <div>Loading...</div>;
+  }
+
+  if (error) {
+    return <div>Error: {error}</div>;
+  }
+  return (
+    <div className="labs_resources__container">
+      <div className="labs__header">
+        <h1>Labs Resources</h1>
+      </div>
+      <div className="labs__content">
+        {labs?.map((lab) => {
+          return <LabResourceCard lab={lab} />;
+        })}
+      </div>
+    </div>
+  );
+};
+
+const LabResourceCard = ({ lab }) => {
+  return (
+    <div className="lab__card">
+      <h2>{lab?.name}</h2>
+      <ul>
+        {lab?.resources?.map((resource) => {
+          return (
+            <li key={resource.resourceId}>
+              {resource.resourceName} : {resource.quantity}
+            </li>
+          );
+        })}
+      </ul>
+    </div>
+  );
+};
+
+export default LabsResources;
